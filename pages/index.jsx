3f076a0d41dbb0a99776dde695d8ce811d5f5f90
Giovanni <me@giovannibono.com>
2024-09-18 22:15:22 +0200

    implemented helmet

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,18 @@
 import { Index } from '@/page-components/Index';
+import { Helmet } from 'react-helmet';
+
+
 
 const IndexPage = () => {
-  return <Index />;
+  return (
+  <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>cookiescracking</title>
+      <link rel="canonical" href="https://cookiescracking.giovannibono.com/" />
+    </Helmet>
+    <Index />
+  </>);
 };
 
 export default IndexPage;
